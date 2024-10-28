ca980c2c87ccadaa18216091e657c48b69cee063
AbayTesfaye <abaytesfaye02@gmail.com>
2024-10-28 14:59:46 -0700

    Day 3: Create seeker-registery form

--- resources/views/users/index.blade.php
+++
@@ -1,4 +0,0 @@
-@extends('layout.app')
-@section('content')
-  <h1>Hello layout</h1>
-@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,9 @@
 <?php
-
-use App\Http\Controllers\ContactController;
-use App\Http\Controllers\TestController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/about', function(){
-    return view('users.index');
-});
+Route::get('/seeker-registery',[UserController::class, 'createSeeker']);
