0e58c4e2112f904fbe7128c0549993e781f2a19c
AbayTesfaye <abaytesfaye02@gmail.com>
2024-10-30 06:03:48 -0700

    show user trial was/will expire message

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -3,7 +3,7 @@
     <div class="container mt-s">
         Hello, {{auth()->User()->name}}
         @if (Auth::check() && auth()->user()->user_type == 'Employer')
-        <p>Your trial will be expire on {{auth()->user()->user_trial}}</p>
+        <p>Your trial {{now()->format('Y-m-d') > auth()->user()->user_trial ? 'was expired' : 'will expire' }} on {{auth()->user()->user_trial}}</p>
         @endif
         <div class="row justify-content-center ">
            <div class="col-md-3">
