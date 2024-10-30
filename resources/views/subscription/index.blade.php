d25bb9d812fa6212e778a12b2af2d1bccd7c9b5e
AbayTesfaye <abaytesfaye02@gmail.com>
2024-10-30 15:48:31 -0700

    add bootstrap cards to the subscription page

--- resources/views/subscription/index.blade.php
+++ resources/views/subscription/index.blade.php
@@ -3,13 +3,61 @@
     <div class="container mt-5">
         <div class="row">
             <div class="col-md-4">
-                col 1
+                <div class="card" style="width: 18rem;">
+                    <div class="card-body">
+                        <h5 class="card-title">Weekly - 20$</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
+                            card's content.</p>
+                    </div>
+                    <ul class="list-group list-group-flush">
+                        <li class="list-group-item">Cras justo odio</li>
+                        <li class="list-group-item">Dapibus ac facilisis in</li>
+                        <li class="list-group-item">Vestibulum at eros</li>
+                    </ul>
+                    <div class="card-body">
+                        <a href="#" class="card-link">
+                            <button class="btn btn-success">Pay</button>
+                        </a>
+                    </div>
+                </div>
             </div>
             <div class="col-md-4">
-                col 2
+                <div class="card" style="width: 18rem;">
+                    <div class="card-body">
+                        <h5 class="card-title">Monthly - 50$</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
+                            card's content.</p>
+                    </div>
+                    <ul class="list-group list-group-flush">
+                        <li class="list-group-item">Cras justo odio</li>
+                        <li class="list-group-item">Dapibus ac facilisis in</li>
+                        <li class="list-group-item">Vestibulum at eros</li>
+                    </ul>
+                    <div class="card-body">
+                        <a href="#" class="card-link">
+                            <button class="btn btn-success">Pay</button>
+                        </a>
+                    </div>
+                </div>
             </div>
             <div class="col-md-4">
-                col 3
+                <div class="card" style="width: 18rem;">
+                    <div class="card-body">
+                        <h5 class="card-title">Yearly - 300$</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
+                            card's content.</p>
+                    </div>
+                    <ul class="list-group list-group-flush">
+                        <li class="list-group-item">Cras justo odio</li>
+                        <li class="list-group-item">Dapibus ac facilisis in</li>
+                        <li class="list-group-item">Vestibulum at eros</li>
+                    </ul>
+                    <div class="card-body">
+                        <a href="#" class="card-link">
+                            <button class="btn btn-success">Pay</button>
+                        </a>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
