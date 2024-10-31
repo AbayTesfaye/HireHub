7041a3604edf7f32bd20d43f5ef1d7347ecfb9f9
AbayTesfaye <abaytesfaye02@gmail.com>
2024-10-31 15:03:17 -0700

    work for initiatePayement function inside SubscriptionController

--- app/Http/Controllers/SubscriptionController.php
+++ app/Http/Controllers/SubscriptionController.php
@@ -6,11 +6,52 @@ use Illuminate\Http\Request;
 
 class SubscriptionController extends Controller
 {
+    const WEEKLY_AMOUNT = "20$";
+    const MONTHLY_AMOUNT = "80$";
+    const YEARLY_AMOUNT = "200$";
+    const CURRENCY = 'USD';
+
     public function subscribe(){
         return view('subscription.index');
     }
 
     public function initiatePayement(){
-        dd('okay');
+        $plans = [
+            'weekly' => [
+                'name' => 'weekly',
+                'discription' => 'weekly payment',
+                'ammount' => self::WEEKLY_AMOUNT,
+                'currency'=> self::CURRENCY,
+                'quantity' => 1,
+            ],
+            'monthly' => [
+                'name' => 'monthly',
+                'discription' => 'monthly payment',
+                'ammount' => self::MONTHLY_AMOUNT,
+                'currency'=> self::CURRENCY,
+                'quantity' => 1,
+            ],
+            'yearly' => [
+                'name' => 'yearly',
+                'discription' => 'yearly payment',
+                'ammount' => self::YEARLY_AMOUNT,
+                'currency'=> self::CURRENCY,
+                'quantity' => 1,
+            ]
+        ];
+
+        // initiate payment
+      try{
+
+      } catch(\Exception $e){
+
+      }
+    }
+
+    public function paymentSuccess(){
+        // update db
+    }
+    public function cancel(){
+        // redirect
     }
 }
