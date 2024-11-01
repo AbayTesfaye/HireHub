45afc48df3479a5a340c82b1965a0e9467c03e5a
AbayTesfaye <abaytesfaye02@gmail.com>
2024-11-01 14:53:42 -0700

    make visible dashboard and subscribe to the user

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -1,57 +1,70 @@
 <!doctype html>
 <html lang="en">
-  <head>
+
+<head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Jobs Search</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-  </head>
-  <body>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+
+<body>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
+    </script>
     <nav class="navbar bg-dark border-bottom navbar-expand-lg border-body" data-bs-theme="dark">
         <div class="container">
-          <a class="navbar-brand" href="#">Navbar</a>
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav ms-auto">
-              <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="#">Home</a>
-              </li>
-            @if (!Auth::check())
-            <li class="nav-item">
-                <a class="nav-link" href="{{route('login')}}">Login</a>
-              </li>
+            <a class="navbar-brand" href="#">Navbar</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav ms-auto">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="{{ route('dashboard') }}">Dashboard</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="{{ route('subscribe') }}">Subscribe</a>
+                    </li>
+                    @if (!Auth::check())
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('login') }}">Login</a>
+                        </li>
 
-                <li class="nav-item">
-                    <a class="nav-link" href="{{route('create.seeker')}}">Job Seeker</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="{{route('create.employer')}}">Job Employer</a>
-                </li>
-            @endif
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('create.seeker') }}">Job Seeker</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('create.employer') }}">Job Employer</a>
+                        </li>
+                    @endif
 
-            @if (Auth::check())
-            <li class="nav-item">
-                <a class="nav-link" id="logout" href="#">Logout</a>
-              </li>
-            @endif
-              <form id="form-logout" action="{{route('logout')}}" method="post">@csrf</form>
-            </ul>
-          </div>
+                    @if (Auth::check())
+                        <li class="nav-item">
+                            <a class="nav-link" id="logout" href="#">Logout</a>
+                        </li>
+                    @endif
+                    <form id="form-logout" action="{{ route('logout') }}" method="post">@csrf</form>
+                </ul>
+            </div>
         </div>
-      </nav>
+    </nav>
     @yield('content')
 
 
     <script>
         let logout = document.getElementById('logout');
         let form = document.getElementById('form-logout');
-        logout.addEventListener('click', function(event){
+        logout.addEventListener('click', function(event) {
             event.preventDefault();
             form.submit();
         });
     </script>
-  </body>
+</body>
+
 </html>
