68a0406ebb384a1e9e4238dca000617045a34af4
AbayTesfaye <abaytesfaye02@gmail.com>
2024-11-05 15:26:03 -0800

    add modal bootstrap for delete link

--- resources/views/job/index.blade.php
+++ resources/views/job/index.blade.php
@@ -32,8 +32,32 @@
                                     <td>{{ $job->title }}</td>
                                     <td>{{ $job->created_at->format('Y-m-d') }}</td>
                                     <td><a href="{{ route('job.edit', [$job->id]) }}">Edit</a></td>
-                                    <td>Delete</td>
+                                    <td><a href="#" data-bs-toggle="modal"
+                                            data-bs-target="#deleteModal-{{ $job->id }}">Delete</a></td>
                                 </tr>
+                                <!-- Modal -->
+                                <div class="modal fade" id="deleteModal-{{ $job->id }}" tabindex="-1" role="dialog"
+                                    aria-labelledby="exampleModalLabel" aria-hidden="true">
+                                    <div class="modal-dialog" role="document">
+                                        <div class="modal-content">
+                                            <div class="modal-header">
+                                                <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                                                <button type="button" class="close" data-dismiss="modal"
+                                                    aria-label="Close">
+                                                    <span aria-hidden="true">&times;</span>
+                                                </button>
+                                            </div>
+                                            <div class="modal-body">
+                                                ...
+                                            </div>
+                                            <div class="modal-footer">
+                                                <button type="button" class="btn btn-secondary"
+                                                    data-bs-dismiss="modal">Close</button>
+                                                <button type="button" class="btn btn-primary">Save changes</button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
                             @endforeach
                         </tbody>
                     </table>
