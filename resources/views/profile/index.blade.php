c03d2b6b9f1ef137c72a9703c04b983814bc669c
AbayTesfaye <abaytesfaye02@gmail.com>
2024-11-05 18:27:36 -0800

    form for user profile update

--- resources/views/profile/index.blade.php
+++ resources/views/profile/index.blade.php
@@ -2,7 +2,19 @@
 @section('content')
     <div class="container mt-s">
         <div class="col-md-8 justify-content-center">
-            <h1>This is user profile setting</h1>
+            <form action="{{ route('user.update.profile') }}" method="POST">
+                <div class="form-group mt-4">
+                    <label for="logo">Logo</label>
+                    <input type="file" name="profile_pic" id="logo" class="form-control">
+                </div>
+                <div class="form-group">
+                    <label for="name">Company Name</label>
+                    <input name="name" id="name" class="form-control">
+                </div>
+                <div class="form-group mt-4">
+                    <button class="btn btn-success" type="submit">Update</button>
+                </div>
+            </form>
         </div>
     </div>
 @endsection
